224341985a551dd188524b6b9d884774c0cf4180
borishangh <borishan.gh@gmail.com>
2024-10-15 16:33:06 +0530

    created reduced dsets + preliminary classif

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Detecting Globular Clusters from GAIA data release
+
+## Reference
+
+- Iván H Bustos Fierro, J H Calderón, Extraction of globular clusters members with Gaia DR2 astrometry, Monthly Notices of the Royal Astronomical Society, Volume 488, Issue 3, September 2019, Pages 3024–3034, (https://doi.org/10.1093/mnras/stz1879)[https://doi.org/10.1093/mnras/stz1879]
---
+++ lib/catalog.py
@@ -0,0 +1,43 @@
+# copied from ChatGPT
+catalog = {
+    "NGC_1261": {
+        "ra": "03h12m16.21s",
+        "dec": -55.2170,
+        "tidal_radius": 15.8  # Tidal radius in arcminutes
+    },
+    "NGC_3201": {
+        "ra": "10h17m36.82s",
+        "dec": -46.4111,
+        "tidal_radius": 28.3
+    },
+    "NGC_6139": {
+        "ra": "16h27m40.36s",
+        "dec": -38.8481,
+        "tidal_radius": 13.2
+    },
+    "NGC_6205": {
+        "ra": "16h41m41.24s",
+        "dec": 36.4603,
+        "tidal_radius": 21.0
+    },
+    "NGC_6362": {
+        "ra": "17h31m54.99s",
+        "dec": -67.0483,
+        "tidal_radius": 15.4
+    },
+    "NGC_6397": {
+        "ra": "17h40m42.09s",
+        "dec": -53.6743,
+        "tidal_radius": 9.9
+    },
+    "NGC_6712": {
+        "ra": "18h53m04.30s",
+        "dec": -8.7058,
+        "tidal_radius": 16.0
+    },
+    "Pal_13": {
+        "ra": "23h06m44.98s",
+        "dec": 12.7719,
+        "tidal_radius": 1.3
+    }
+}
